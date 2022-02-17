b0b5ba6a3f3cf818de7c41383cf3c86ca099c089
Anh Le <a.v.le@wustl.edu>
2022-02-16 23:23:07 -0600

    Layout for signup/login page added

--- frontend/src/components/route/Login.jsx
+++ frontend/src/components/route/Login.jsx
@@ -1,10 +1,36 @@
 import AuthNavigationBar from "../AuthNavBar";
+import { useState } from "react";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLoginUser = async () => {
+        console.log("Login Pressed");
+    }
     return (
         <div>
             <AuthNavigationBar />
-            <p>Login Page</p>
+            <div className="center">
+                <p>Email</p>
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                    name="email" placeholder="Email" required
+                />
+
+                <p>Password</p>
+                <input type="password"
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    name="password" placeholder="Password" required
+                />
+                <br />
+                <button onClick={handleLoginUser} className="main_button" id="signup_btn">Sign Up</button>
+            </div>
         </div>
     )
 }
--- frontend/src/components/route/Signup.jsx
+++ frontend/src/components/route/Signup.jsx
@@ -1,10 +1,59 @@
 import AuthNavigationBar from "../AuthNavBar";
+import { useState } from "react";
 
 function Signup() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setlastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleRegisterUser = async () => {
+        console.log("Signup Pressed");
+    }
+
     return (
         <div>
             <AuthNavigationBar />
-            <p>Signup Page</p>
+            <div className="center">
+                <p>First Name</p>
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setFirstName(e.target.value);
+                    }}
+                    name="first_name" placeholder="First Name" required
+                />
+
+                <p>Last Name</p>
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setlastName(e.target.value);
+                    }}
+                    name="last_name"
+                    placeholder="Last Name"
+                    required
+                />
+
+                <p>Email</p>
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                    name="email" placeholder="Email" required
+                />
+
+                <p>Password</p>
+                <input type="password"
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    name="password" placeholder="Password" required
+                />
+                <br />
+                <button onClick={handleRegisterUser} className="main_button" id="signup_btn">Sign Up</button>
+            </div>
         </div>
     )
 }
