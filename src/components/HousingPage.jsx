1f32bb7a2dc9a046c00f578851d7b8e0dd416953
Kien Ta <talekien1710@gmail.com>
2022-03-07 23:14:56 -0600

    login/signup stay

--- src/components/HousingPage.jsx
+++ src/components/HousingPage.jsx
@@ -1,6 +1,20 @@
 import NavigationBar from "./NavigationBar";
+import UserContext from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
 
 function Housing() {
+    let navigate = useNavigate();
+    let { currUser, setUser } = useContext(UserContext);
+
+    useEffect(() => {
+        (async () => {
+            if (!currUser) {
+                navigate("/");
+            }
+        })();
+    }, [currUser]);
+
     return (
         <div>
             <NavigationBar />
