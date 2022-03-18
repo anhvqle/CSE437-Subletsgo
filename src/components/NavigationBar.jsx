4c996539a052c57775b25811fffe5b11506b3a7a
Anh Le <a.v.le@wustl.edu>
2022-03-18 18:44:55 -0500

    Fix NavBar Styling - highlight on select

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,7 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import UserContext from "../context/UserContext"
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 
 const AuthNavigationBar = () => {
 
@@ -17,23 +17,23 @@ const AuthNavigationBar = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand className="auth-navbar-title" href="/">Subletsgo</Navbar.Brand>
+                <Navbar.Brand className="navbar-title" href="/">Subletsgo</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto"></Nav>
                     {
                         currUser && (<Nav className="me-auto d-flex flex-row">
-                            <Nav.Link className="auth-navbar" href="/tenant">Tenant</Nav.Link>
-                            <Nav.Link className="auth-navbar" href="/housing">Housing</Nav.Link>
-                            <Nav.Link className="auth-navbar" href="/marketplace">Marketplace</Nav.Link>
+                            <NavLink className={({isActive}) => isActive ? "active" : "nav-item"} to="/tenant">Tenant</NavLink>
+                            <NavLink className={({isActive}) => isActive ? "active" : "nav-item"} to="/housing">Housing</NavLink>
+                            <NavLink className={({isActive}) => isActive ? "active" : "nav-item"} to="/marketplace">Marketplace</NavLink>
                         </Nav>)
                     }
                     {
                         currUser
                             ? <button className="btn btn-outline-light btn-lg" onClick={logoutClicked}>Logout</button>
                             : (<Nav>
-                                <Nav.Link className="auth-navbar" href="/signup">Signup</Nav.Link>
-                                <Nav.Link className="auth-navbar" href="/login">Login</Nav.Link>
+                                <NavLink className={({isActive}) => isActive ? "active" : "nav-item"} to="/signup">Signup</NavLink>
+                                <NavLink className={({isActive}) => isActive ? "active" : "nav-item"} to="/login">Login</NavLink>
                             </Nav>)
                     }
                 </Navbar.Collapse>
