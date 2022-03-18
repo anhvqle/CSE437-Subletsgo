45f0aa97d42cb313dd2ee7706309f76b3d4cfa08
Kien Ta <talekien1710@gmail.com>
2022-03-18 00:42:32 -0500

    house listing done

--- src/components/housing/HousingListing.jsx
+++ src/components/housing/HousingListing.jsx
@@ -1,10 +1,31 @@
-const HousingListing = ({ housings }) => {
+import { Container, Row, Col } from "react-bootstrap";
 
-    return (
-        <div>
-            <p>Housing Listings here</p>
-        </div >
-    )
+const HousingListing = ({ housings }) => {
+    const Housing = ({ housing }) => {
+        return (
+            <Container fluid>
+                <Row className="layout">
+                    <Col sm={4}>
+                        <img src={housing["housing-images"]} alt={housing.id} width="200" />
+                    </Col>
+                    <Col sm={8}>
+                        <div className="medium_size"><strong>Price: </strong> {housing.price}</div>
+                        <div className="medium_size"><strong>Number of Bed: </strong> {housing.numBed}</div>
+                        <div className="medium_size"><strong>Number of Bath:</strong> {housing.numBath}</div>
+                        <div className="medium_size"><strong>Air Conditioner:</strong> {housing.airConditioner === 1 ? "Yes" : "No"}</div>
+                        <div className="medium_size"><strong>Laundry:</strong> {housing.laundry === 1 ? "Yes" : "No"}</div>
+                        <div className="medium_size"><strong>Balcony:</strong> {housing.balcony === 1 ? "Yes" : "No"}</div>
+                        <div className="medium_size"><strong>Pet Friendly:</strong> {housing.petFriendly === 1 ? "Yes" : "No"}</div>
+                        <div className="medium_size"><strong>Elevator:</strong> {housing.elevator === 1 ? "Yes" : "No"}</div>
+                        {housing['housing-address'] && <div className="medium_size"><strong>Address:</strong> {housing['housing-address'].label}</div>}
+                    </Col>
+                </Row>
+                <hr />
+            </Container>
+        )
+    }
+
+    return housings.map(housing => <Housing housing={housing} key={housing.id} />)
 }
 
 export default HousingListing;
